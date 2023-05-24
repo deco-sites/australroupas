55b28592c76cdfa2e67901ee4c2b3e9a3f1f9f4a
jovenan <jovenan2001@gmail.com>
2023-05-23 21:42:41 -0300

    config cart URL

--- components/minicart/Cart.tsx
+++ components/minicart/Cart.tsx
@@ -15,8 +15,7 @@ declare global {
   }
 }
 
-const CHECKOUT_URL =
-  "https://austral.vtexcommercestable.com.br/checkout";
+const CHECKOUT_URL = "https://austral.vtexcommercestable.com.br/checkout";
 
 function Cart() {
   const { displayCart } = useUI();
