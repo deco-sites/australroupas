e0c7320b5134069e06a0f282dad32ef34436b56e
jovenan <jovenan2001@gmail.com>
2023-06-09 22:40:04 -0300

    deno check

--- components/ui/Magazine.tsx
+++ components/ui/Magazine.tsx
@@ -1,5 +1,12 @@
 export default function Magazine() {
   return (
-    <iframe style={{height: "100vh", marginTop: "-100px"}} src="https://www.magazine.austral.com.br" frameBorder="0" width={"100%"} height={"100%"}></iframe>
+    <iframe
+      style={{ height: "100vh", marginTop: "-100px" }}
+      src="https://www.magazine.austral.com.br"
+      frameBorder="0"
+      width={"100%"}
+      height={"100%"}
+    >
+    </iframe>
   );
 }
