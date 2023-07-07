f73bbae847ff64671c097ca03401231b3e463f8c
guitavano <tavano62@gmail.com>
2023-07-06 22:43:38 -0300

    fix dots

--- components/product/ProductShelf.tsx
+++ components/product/ProductShelf.tsx
@@ -33,13 +33,13 @@ function ProductShelf({
   const gapDesktop = twoItemsPerPage ? "gap-[2.5%]" : "gap-[1.33%]";
 
   const gridRows = title
-    ? "grid-rows-[64px_1fr_48px_1fr] py-10"
+    ? "grid-rows-[64px_1fr_48px_1fr] pt-10"
     : "grid-rows-[1fr]";
 
   return (
     <div
       id={id}
-      class={`sm:home-container relative grid grid-cols-[48px_1fr_48px] ${gridRows} px-0 ${paddingXitemsDesktop} my-10 mb-10`}
+      class={`sm:home-container relative grid grid-cols-[48px_1fr_48px] ${gridRows} px-0 ${paddingXitemsDesktop} my-10 mb-10 pb-10`}
     >
       {title &&
         (
@@ -49,7 +49,7 @@ function ProductShelf({
         )}
 
       <Slider
-        class={`carousel carousel-center sm:carousel-end col-span-full row-start-2 row-end-5 gap-4 sm:${twoItemsPerPage}`}
+        class={`carousel carousel-center sm:carousel-end col-span-full row-start-2 row-end-5 gap-4 sm:${gapDesktop}`}
       >
         {products?.map((product, index) => (
           <Slider.Item
