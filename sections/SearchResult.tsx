ba0e5b7627a58b3806a90680d4952178d8e7d6f0
jovenan <jovenan2001@gmail.com>
2023-07-08 19:39:52 -0300

    feat: busca vazia

--- sections/SearchResult.tsx
+++ sections/SearchResult.tsx
@@ -1,1 +1,26 @@
-export { default } from "$store/components/search/SearchResult.tsx";
+import SearchResult, {
+  MainProps,
+  Props,
+} from "$store/components/search/SearchResult.tsx";
+
+function SearchResultSection(props: MainProps) {
+  return <SearchResult {...props} />;
+}
+
+export default SearchResultSection;
+
+export function loader(props: Props, req: Request) {
+  const url = new URL(req.url);
+
+  if (new URLPattern({ pathname: "/s" }).test(url)) {
+    return {
+      ...props,
+      headingText: url.searchParams.get("q") ?? "",
+    };
+  }
+
+  return {
+    ...props,
+    headingText: "",
+  };
+}
