5f8b583f4e017eda7faa5bcf55dd10e17870a5e5
guitavano <tavano62@gmail.com>
2023-08-11 19:43:02 -0300

    back dev.ts

--- dev.ts
+++ dev.ts
@@ -1,6 +1,11 @@
 #!/usr/bin/env -S deno run -A --watch
 import dev from "$live/dev.ts";
-import site from "./site.json" assert { type: "json" };
+import liveManifest from "$live/live.gen.ts";
+import liveStdManifest from "deco-sites/std/live.gen.ts";
 
-// Generate manifest and boot server
-await dev(import.meta.url, "./main.ts", { ...site, injectRoutes: false });
+await dev(import.meta.url, "./main.ts", {
+  imports: {
+    "$live": liveManifest,
+    "deco-sites/std": liveStdManifest,
+  },
+});
