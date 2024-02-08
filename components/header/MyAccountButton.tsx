999e436a330882e6a838b5279c450a1cf2dd0302
pandero7 <rafasoli10@gmail.com>
2024-02-08 19:44:01 -0300

    feat: logged popup

--- components/header/MyAccountButton.tsx
+++ components/header/MyAccountButton.tsx
@@ -9,8 +9,7 @@ const MyAccountButton = () => {
   return (
     <>
     <a
-      class="p-2.5 lg:ml-7.5 relative"
-      href="#"
+      class="p-2.5 lg:ml-7.5 relative cursor-pointer"
       aria-label="Log in"
       onClick={ (e) => {
         e.preventDefault();
@@ -24,34 +23,41 @@ const MyAccountButton = () => {
       </i>
     </a>
     <div class={`${displayModalLogin.value ? "flex" : "hidden"} flex-col popupLogin lg:right-[77px] lg:mt-5 lg:before:right-4`}>
-        <p class={`text-black text-sm font-bold pb-[22px] mb-2.5 border-b border-[#C7C7C7]`}>Olá! Seja bem vindo!</p>
-        <a href="#"
-        class={`text-primary text-sm underline py-2.5`}
-        onClick={async (e) => {
-          e.preventDefault();
-          if (user.value?.email) {
-            window.location.pathname = "/account";
-          } else {
-            const execute = () => {
-              vtexIdScriptsLoaded.value = true;
-              // deno-lint-ignore ban-ts-comment
-              // @ts-expect-error
-              window.vtexid.start({
-                userEmail: "",
-                locale: "pt-BR",
-                forceReload: true,
-              });
-            };
-            if (!vtexIdScriptsLoaded.value) {
-              const { loadVtexIdScripts } = await import(
-                "deco-sites/australroupas/sdk/loadVtexIdScripts.ts"
-              );
-              loadVtexIdScripts(execute);
-            } else {
-              execute();
-            }
-          }
-        }}>Entre ou cadastre-se</a>
+      {!user.value?.email ? 
+        <>
+          <p class={`text-black text-sm font-bold pb-[22px] mb-2.5 border-b border-[#C7C7C7]`}>Olá! Seja bem vindo!</p>
+          <a class={`text-primary text-sm underline py-2.5`}
+          onClick={async (e) => {
+            e.preventDefault();
+              const execute = () => {
+                vtexIdScriptsLoaded.value = true;
+                // deno-lint-ignore ban-ts-comment
+                // @ts-expect-error
+                window.vtexid.start({
+                  userEmail: "",
+                  locale: "pt-BR",
+                  forceReload: true,
+                });
+              };
+              if (!vtexIdScriptsLoaded.value) {
+                const { loadVtexIdScripts } = await import(
+                  "deco-sites/australroupas/sdk/loadVtexIdScripts.ts"
+                );
+                loadVtexIdScripts(execute);
+              } else {
+                execute();
+              }
+          }}>Entre ou cadastre-se</a>
+        </>
+        :
+        <>
+          <p class={`text-black text-sm font-bold pb-[22px] mb-2.5 border-b border-[#C7C7C7]`}>Olá, ${user.value?.email}!</p>
+          <a href="/account" class={`lg:hover:underline py-2.5`}>Minha conta</a>
+          <a href="/account#orders" class={`lg:hover:underline py-2.5`}>Meus pedidos</a>
+          <a href="/no-cache/user/logout?returnUrl=/" class={`lg:hover:underline py-2.5 font-bold`}>Sair</a>
+        </>
+      }
+        
         <Button
           class="p-2.5 text-xl lg:hidden absolute top-0 right-0"
           onClick={(e) => {
