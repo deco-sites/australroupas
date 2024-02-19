965681d17ae06bfebe8eebacab271430fc01b038
pandero7 <rafasoli10@gmail.com>
2024-02-19 17:16:53 -0300

    fix: btn hover color

--- components/ui/ButtonSize.tsx
+++ components/ui/ButtonSize.tsx
@@ -22,7 +22,7 @@ export default function ButtonSize({ title, btnsLetters, btnsNumbers, title2, la
           {btnsLetters.map((item) => {
             return(
               <li class={``}>
-                <a class={`hover:bg-black hover:text-white w-[40vw] md:w-full lg:w-[10.7vw] lg:max-w-[165px] min-w-[160px] md:min-w-[100px] text-base flex justify-center items-center px-4 text-black h-[59px] lg:h-[52px] border-2 border-black rounded-md`} href={item.href}>
+                <a class={`hover:bg-primary hover:border-primary hover:text-white w-[40vw] md:w-full lg:w-[10.7vw] lg:max-w-[165px] min-w-[160px] md:min-w-[100px] text-base flex justify-center items-center px-4 text-black h-[59px] lg:h-[52px] border-2 border-black rounded-md`} href={item.href}>
                   {item.label}
                 </a>
               </li>
@@ -33,7 +33,7 @@ export default function ButtonSize({ title, btnsLetters, btnsNumbers, title2, la
           {btnsNumbers.map((item) => {
             return(
               <li class={`first-of-type:mt-5 md:first-of-type:mt-0`}>
-                <a class={`hover:bg-black hover:text-white w-[40vw] md:w-full lg:w-[10.7vw] lg:max-w-[165px] min-w-[160px] md:min-w-[100px] text-base flex justify-center items-center px-4 text-black h-[59px] lg:h-[52px] border-2 border-black rounded-md`} href={item.href}>
+                <a class={`hover:bg-primary hover:border-primary hover:text-white w-[40vw] md:w-full lg:w-[10.7vw] lg:max-w-[165px] min-w-[160px] md:min-w-[100px] text-base flex justify-center items-center px-4 text-black h-[59px] lg:h-[52px] border-2 border-black rounded-md`} href={item.href}>
                   {item.label}
                 </a>
               </li>
@@ -41,7 +41,7 @@ export default function ButtonSize({ title, btnsLetters, btnsNumbers, title2, la
           })}
         </ul>
         <h2 class={`text-xl text-black mt-10 lg:text-2.5xl`}>{title2}</h2>
-        <a class={`hover:bg-black hover:text-white w-[40vw] md:w-[160px] lg:w-[10.7vw] lg:max-w-[165px] min-w-[160px] md:min-w-[100px] text-base flex justify-center items-center px-4 text-black h-[59px] lg:h-[52px] border-2 border-black rounded-md`} href={lastButton.href}>
+        <a class={`hover:bg-primary hover:border-primary hover:text-white w-[40vw] md:w-[160px] lg:w-[10.7vw] lg:max-w-[165px] min-w-[160px] md:min-w-[100px] text-base flex justify-center items-center px-4 text-black h-[59px] lg:h-[52px] border-2 border-black rounded-md`} href={lastButton.href}>
           {lastButton.label}
         </a>
       </div>
