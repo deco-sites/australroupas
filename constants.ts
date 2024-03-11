59e623840f3daed44dc4b8b203d898060c09c2a3
Gustavo Vasconcellos <pvasconcellos.gustavo@gmail.com>
2024-03-11 20:05:59 -0300

    fix: change constant to secure.

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
-export const PORTAL_SUBDOMAIN = ["https://www.austral.com.br"];
+export const PORTAL_SUBDOMAIN = ["https://secure.austral.com.br"];
 export const PORTAL_APIDOMAIN = ["https://austral.vtexcommercestable.com.br"];
